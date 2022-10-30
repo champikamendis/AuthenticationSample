94bfb78f0eb41ada89417f87b9a0a50401e7d794
Champika Mendis <champikamendis.cm@gmail.com>
2022-10-30 22:57:26 +0530

    code refactored and jest added

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: 'User',
+          useValue: User,
+        },
+        AuthService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('register', () => {
+    it('should register a new user to the system', async () => {
+      const user = {
+        _id: '635e2e57cbb98e6a60cbfcdf',
+        email: 'champikamendis2@gmail.com',
+        __v: 0,
+      };
+      const result = [{ user }];
+      jest.spyOn(service, 'create').mockImplementation(async () => {
+        return user;
+      });
+
+      expect(await controller.register).toBe(result);
+    });
   });
 });
